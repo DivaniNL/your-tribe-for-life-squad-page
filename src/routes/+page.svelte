322d07faeb3309550508ee3c741a4be5baf2b13d
Colin de Groot <colindegroot2004@gmail.com>
2025-09-03 22:18:26 +0200

    basic styling en kleine fix voor sliders #18  #14
    ul stond in de each loop ...oops. er staat nu een hele simpele layout voor de members

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,47 +3,74 @@
   const members = data.members;
   console.log(data);
 
- // split members in half
+  // split members in half
   const splitMembers = Math.ceil(members.length / 2);
 
   // make variable for each slide
   const firstHalf = members.slice(0, splitMembers); //first half
   const secondHalf = members.slice(splitMembers); //second half
-
 </script>
 
 <h1>FDND Squadpage</h1>
 
 <fieldset>
-    <legend>placeholder voor de "filters"</legend>
-    <a href="/">Keuze1</a>
+  <legend>placeholder voor de "filters"</legend>
+  <a href="/">Keuze1</a>
 </fieldset>
 
 <!-- top slider -->
-<div class="slider">
-  {#each firstHalf as member}
-    <ul>
-      <li class="person-card">
-        <img src={member.avatar} alt="" />
-        <span><p>Show item</p></span>
-        <!-- <span>{member[filteredItem]}</span> -->
-        <h3>{member.name}</h3>
-        <a href={member.website}>github logo</a>
-      </li>
-    </ul>
-  {/each}
-</div>
+ <ul class="slider">
+    {#each firstHalf as member}
+    <li class="person-card">
+      <img src={member.avatar} alt="" />
+      <span><p>Show item</p></span>
+      <!-- <span>{member[filteredItem]}</span> -->
+      <h3>{member.name}</h3>
+      <a href={member.website}>github logo</a>
+    </li>
+    {/each}
+  </ul>
 
 <!-- bottom slider -->
-<div class="slider" style="border: pink solid 2px;">
-  {#each secondHalf as member}
-    <ul>
-      <li class="person-card">
-        <img src={member.avatar} alt="" />
-        <h3>{member.name}</h3>
-        <a href={member.website}>github logo</a>
-      </li>
-    </ul>
-  {/each}
-</div>
+ 
+<ul class="slider">
+    {#each secondHalf as member}
+    <li class="person-card">
+    <span><p>Show item</p></span>
+      <img src={member.avatar} alt="" />
+      <h3>{member.name}</h3>
+      <a href={member.website}>github logo</a>
+    </li>
+    {/each}
+</ul>
 
+<style>
+    ul{
+        width: auto;
+        display: flex;
+        flex-flow: row wrap;
+        justify-content: center;
+    }
+
+  .person-card {
+    position: relative;
+    max-height: 30em;
+    width: 30em;
+    overflow: hidden; /* voorkomt dat img buiten de card steekt */
+
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    padding: 1rem;
+    color: white;
+  }
+
+  .person-card img {
+    position: absolute;
+    inset: 0; /* top:0; right:0; bottom:0; left:0 */
+    width: 100%;
+    height: 100%;
+    object-fit: cover; /* zorgt dat de foto het vak mooi opvult */
+    z-index: -1; /* achter de content */
+  }
+</style>
