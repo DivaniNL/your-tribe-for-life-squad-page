857c6962753abbc571dc2e24bc7034a18d9fae8d
Colin de Groot <colindegroot2004@gmail.com>
2025-09-03 21:49:12 +0200

    basis opzet html af #18

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,20 +2,48 @@
   let { data } = $props();
   const members = data.members;
   console.log(data);
-</script>
 
+ // split members in half
+  const splitMembers = Math.ceil(members.length / 2);
+
+  // make variable for each slide
+  const firstHalf = members.slice(0, splitMembers); //first half
+  const secondHalf = members.slice(splitMembers); //second half
+
+</script>
 
 <h1>FDND Squadpage</h1>
 
-<p>hier komen filters</p>
+<fieldset>
+    <legend>placeholder voor de "filters"</legend>
+    <a href="/">Keuze1</a>
+</fieldset>
 
-{#each members as member}
-  <ul>
-    <li class="person-card">
-        <img src="{member.avatar}" alt="">
+<!-- top slider -->
+<div class="slider">
+  {#each firstHalf as member}
+    <ul>
+      <li class="person-card">
+        <img src={member.avatar} alt="" />
+        <span><p>Show item</p></span>
+        <!-- <span>{member[filteredItem]}</span> -->
         <h3>{member.name}</h3>
-        <a href="{member.website}">github logo</a>
-    </li>
-  </ul>
-{/each}
+        <a href={member.website}>github logo</a>
+      </li>
+    </ul>
+  {/each}
+</div>
+
+<!-- bottom slider -->
+<div class="slider" style="border: pink solid 2px;">
+  {#each secondHalf as member}
+    <ul>
+      <li class="person-card">
+        <img src={member.avatar} alt="" />
+        <h3>{member.name}</h3>
+        <a href={member.website}>github logo</a>
+      </li>
+    </ul>
+  {/each}
+</div>
 
