ee3e324770acc93cbde6aabc7f7a1e75d51f8d4f
vsheo <vireshsheoratan@gmail.com>
2025-09-04 13:24:24 +0200

    Details HTML first #21

---
+++ src/routes/[id]/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+
+	<style>
+	</style>
+</svelte:head>
+
+
+
+{@render children?.()}
--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -1,6 +1,37 @@
 <script>
 	let { data } = $props();
-    const member = data.member
-    console.log(member);
+    const member = data.member;
+    const filters = data.allowedFilters;
+    console.log(filters)
 </script>
-<h1>{member.name}</h1>
+
+<h1>Squad 2F</h1>
+
+<img src="{member.avatar}" alt="avatar image van {member.name}" width="250px" height="250px">
+<h2>{member.name}</h2>
+
+<section class="details-container">
+    <article>
+        <h3>Bio</h3>
+        <p>{member.bio}</p>
+    </article>
+
+    <article>
+        <h3>Links</h3>
+        <a href="https://github.com/{member.github_handle}">Github</a>
+        <a href="{member.website}">website</a>
+    </article>
+</section>
+
+<ul class="Favo-slider">
+    {#each filters as filter}
+    <li>
+        <h3>{filter[1]}</h3>
+        <span>{member[filter[0]]}</span>
+    </li>
+    {/each}
+</ul>
+
+
+
+
