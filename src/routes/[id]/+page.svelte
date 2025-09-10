3236884596673cd88b887db6c53483f1777fa4e2
vsheo <vireshsheoratan@gmail.com>
2025-09-10 11:03:53 +0200

    fallback img view transition #52

--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -189,7 +189,7 @@
     {#if member.mugshot}
         <img class="animation-fade-in" src="https://fdnd.directus.app/assets/{member.mugshot}?width=500&height=500" alt="foto van {member.name}" width=250 height=250 style="view-transition-name: person-{member.id}"/>
     {:else}
-    <img src="https://avatars.githubusercontent.com/u/89637532?s=200&v=4" alt="MONKE" width=250 height=250/>
+    <img src="https://avatars.githubusercontent.com/u/89637532?s=200&v=4" alt="MONKE" width=250 height=250 style="view-transition-name: person-{member.id}"/>
     
     {/if}
 
